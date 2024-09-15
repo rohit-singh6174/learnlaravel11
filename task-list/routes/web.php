a3d056efbe5a0cd3f8e3bdd1c26e197fbea05d8d
Rohit Singh <rohit224455@gmail.com>
2024-09-15 14:21:44 +0545

    Basic Laravel Routing

--- task-list/routes/web.php
+++ task-list/routes/web.php
@@ -5,3 +5,22 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () {
+    return "Hello";
+})-> name('hello');
+
+Route::get('/hi', function() {
+   return redirect()->route('hello'); 
+});
+Route::get('/greet/{name}', function($name){
+    return "Hello $name";
+});
+
+Route::fallback(function (){
+    return "Still got somewhere";
+});
+//GET
+//POST
+//PUT
+//DELETE
